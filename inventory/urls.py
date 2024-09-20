d902d51b99a7644221d3527cb055addc96ba67da
Faisal-Mujtaba-Saleem <faisalmujtaba2005@gmail.com>
2024-09-20 10:58:18 -0700

    Formatted the url patterns

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
     # Items Enpoints.
 
     path('items/', views.list_items, name='list_items'),
@@ -10,7 +9,6 @@ urlpatterns = [
     path('items/retrieve/<str:slug>/', views.retrieve_item, name='retrieve_item'),
     path('items/update/<str:slug>/', views.update_item, name='update_item'),
     path('items/delete/<str:slug>/', views.delete_item, name='delete_item'),
-
 
     # Filter Endpoints
 
@@ -35,24 +33,42 @@ urlpatterns = [
         name='list_items_by_quantity'
     ),
 
-
     # Search Endpoint
 
-    path('items/search/',
-         views.search_items, name='search_items'),
-
+    path(
+        'items/search/',
+        views.search_items,
+        name='search_items'
+    ),
 
     # Category Endpoints
 
-    path('categories/', views.list_categories, name='list_categories'),
-    path('categories/retrieve/<str:slug>/',
-         views.retrieve_category, name='retrieve_category'),
-
+    path(
+        'categories/',
+        views.list_categories,
+        name='list_categories'
+    ),
+    path(
+        'categories/retrieve/<str:slug>/',
+        views.retrieve_category,
+        name='retrieve_category'
+    ),
 
     # Stock Endpoints
 
-    path('stocks/', views.list_stocks, name='list_stocks'),
-    path('stocks/retrieve/<int:item>/',
-         views.retrieve_stock, name='retrieve_stock'),
-    path('stocks/update/<int:item>/', views.update_stock, name='update_stock'),
+    path(
+        'stocks/',
+        views.list_stocks,
+        name='list_stocks'
+    ),
+    path(
+        'stocks/retrieve/<int:item>/',
+        views.retrieve_stock,
+        name='retrieve_stock'
+    ),
+    path(
+        'stocks/update/<int:item>/',
+        views.update_stock,
+        name='update_stock'
+    )
 ]
